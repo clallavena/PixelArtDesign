2edd59b1f17aeb8ca0dafb0f7ef47a6fb7247569
clallavena <clement.allavena@etu.uca.fr>
2019-02-14 18:15:08 +0100

    Ajout de la caméra, quelque bug à noter et à regler: -Deux APK pour l'appli, -Problème de passage sur a vue activity_add;

--- Code/app/src/main/java/info63/iut/pixelartdesign/AddActivity.java
+++ Code/app/src/main/java/info63/iut/pixelartdesign/AddActivity.java
@@ -1,21 +1,21 @@
 package info63.iut.pixelartdesign;
 
-import android.content.Context;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import info63.iut.pixelartdesign.CameraFiles.CameraActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private Intent intentAddActivity;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,14 +24,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setContentView(R.layout.activity_main);
                     return true;
                 case R.id.navigation_creation:
                     mTextMessage.setText(R.string.title_creation);
                     return true;
                 case R.id.navigation_add:
-                    startActivity(intentAddActivity);
-                    mTextMessage.setText(R.string.title_add);
+                    setContentView(R.layout.activity_add);
                     return true;
                 case R.id.navigation_settings:
                     mTextMessage.setText(R.string.title_settings);
@@ -41,17 +40,26 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            Log.d("devNote", "Camera unreachable");
+        }
+        return c; // returns null if camera is unavailable
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        intentAddActivity = new Intent(this, AddActivity.class);
+        setContentView(R.layout.activity_add);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 
     public void addPhoto(View view) {
         Intent intent = new Intent(this, CameraActivity.class);
---
+++ Code/app/src/main/java/info63/iut/pixelartdesign/CameraFiles/CameraActivity.java
@@ -0,0 +1,47 @@
+package info63.iut.pixelartdesign.CameraFiles;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import info63.iut.pixelartdesign.R;
+
+import static info63.iut.pixelartdesign.AddActivity.getCameraInstance;
+
+public class CameraActivity extends AppCompatActivity {
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private int orientationDegree = 90;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+         }
+        mCamera = getCameraInstance();
+
+        mCamera.setDisplayOrientation(orientationDegree);
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCamera.release();
+    }
+}
