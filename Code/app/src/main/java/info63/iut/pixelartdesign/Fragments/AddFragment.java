ae2ba9f9f2486d743e1d0c40925d84b13d6fc88d
clallavena <clement.allavena@etu.uca.fr>
2019-02-21 18:54:35 +0100

    Update Fragment

---
+++ Code/app/src/main/java/info63/iut/pixelartdesign/Fragments/AddFragment.java
@@ -0,0 +1,62 @@
+package info63.iut.pixelartdesign.Fragments;
+
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import info63.iut.pixelartdesign.CameraFiles.CameraActivity;
+import info63.iut.pixelartdesign.R;
+
+public class AddFragment extends Fragment {
+
+    private TextView mTextMessage;
+
+    public static AddFragment newInstance(){
+        return new AddFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        final Button buttonCapture = (Button) view.findViewById(R.id.add_button);
+        buttonCapture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            Log.d("devNote", "Camera unreachable");
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    public void addPhoto(View view) {
+        Intent intent = new Intent(view.getContext(), CameraActivity.class);
+        startActivity(intent);
+    }
+}
